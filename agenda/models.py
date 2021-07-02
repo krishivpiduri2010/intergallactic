308d82f2cfda20f4dff2b3a3085d027a33e036a6
krishivpiduri2010 <krishivpiduri337@gmail.com>
2021-07-02 16:14:46 -0700

    Need to make agenda create view

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Object(models.Model):
+    from_time = models.TimeField()
+    to_time = models.TimeField()
+    name = models.CharField(max_length=100)
+    by=models.ForeignKey('auth.User',on_delete=models.CASCADE)
--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from .models import Object
+
 
 # Create your views here.
+def bubbleSort(arr):
+    n = len(arr)
+
+    # Traverse through all array elements
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j].to_time < arr[j + 1].from_time:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
 def home(request):
-    return render(request,'agenda/home.html')
+    arr=[]
+    for obj in Object.objects.all():
+        arr.append(obj)
+    print(arr)
+    bubbleSort(arr)
+    return render(request, 'agenda/home.html',{'agenda':arr})
